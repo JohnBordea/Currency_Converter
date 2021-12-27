b8993f3a69f86d6f2f312e5b3155b2bb3292b69c
JohnBordea <49273333+JohnBordea@users.noreply.github.com>
2021-12-27 13:56:45 +0200

    Implementata functia save_for_offline_use() in Class-a Converter
    In cazul in care utilizatorul se afla offline, el va fi in stare sa foloseasca aplicatia(dar folosind date vechi). Datele sunt salvate in fisierul exchange_rate.info

--- tools/currency_manager.py
+++ tools/currency_manager.py
@@ -7,17 +7,37 @@ class Converter:
     url = 'https://www.bnr.ro/nbrfxrates.xml'
 
     def __init__(self):
-        response = requests.get(self.url)
-        root = ET.ElementTree( ET.fromstring( response.content ) ).getroot()
+        try:
+            response = requests.get(self.url)
+            root = ET.ElementTree( ET.fromstring( response.content ) ).getroot()
 
-        currency = []
-        for rate in root[1][2]:
-            if 'multiplier' in rate.attrib:
-                currency.append( ( rate.attrib['currency'], (float(rate.text) / float(rate.attrib['multiplier'])) ) )
-            else:
-                currency.append( ( rate.attrib['currency'], float(rate.text) ) )
+            currency = [ (root[1][1].text, 1) ]
+            for rate in root[1][2]:
+                if 'multiplier' in rate.attrib:
+                    currency.append( ( rate.attrib['currency'], (float(rate.text) / float(rate.attrib['multiplier'])) ) )
+                else:
+                    currency.append( ( rate.attrib['currency'], float(rate.text) ) )
 
-        self.exchange_rate = dict(currency)
+            self.exchange_rate = dict(currency)
+            self.save_for_offline_use()
+        except (requests.ConnectionError, requests.Timeout) as exception:
+            print("No internet connection.")
+
+    def save_for_offline_use(self):
+        w = open("data/exchange_rate.info", "w")
+
+        for rate in self.exchange_rate:
+            w.write(f"{rate}={self.exchange_rate[rate]}")
+            if not rate == list( self.exchange_rate.keys() )[-1]:
+                w.write('\n')
+
+        w.close()
+
+    def read_in_case_of_offline(self):
+        f = open("data/exchange_rate.info", encoding="utf8")
+        st = f.read()
+        f.close()
+        return st
 
     def __str__(self):
         st = '{\n'
