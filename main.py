f3bc949af1669e53eac4b9ab58bab7598357dceb
JohnBordea <49273333+JohnBordea@users.noreply.github.com>
2021-12-27 14:55:29 +0200

    Adaugat functii in Converter
    read_in_case_of_offline() -> in cazul in care suntem offline, programul sa fie in stare sa lucreze.
    
    change_from() & change_to() -> schimba baza valutara cu care lucram.

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from tools.currency_manager import Converter
 
 conv = Converter()
 
-#print(conv)
+print(conv.from_currency, conv.to_currency)
--- tools/currency_manager.py
+++ tools/currency_manager.py
@@ -1,10 +1,11 @@
 import requests
 import xml.etree.ElementTree as ET
-import json
 
 class Converter:
     exchange_rate = {}
     url = 'https://www.bnr.ro/nbrfxrates.xml'
+    from_currency = ''
+    to_currency = ''
 
     def __init__(self):
         try:
@@ -21,7 +22,10 @@ class Converter:
             self.exchange_rate = dict(currency)
             self.save_for_offline_use()
         except (requests.ConnectionError, requests.Timeout) as exception:
-            print("No internet connection.")
+            self.read_in_case_of_offline()
+
+        self.from_currency = list(self.exchange_rate.keys())[0]
+        self.to_currency = list(self.exchange_rate.keys())[1]
 
     def save_for_offline_use(self):
         w = open("data/exchange_rate.info", "w")
@@ -34,16 +38,29 @@ class Converter:
         w.close()
 
     def read_in_case_of_offline(self):
-        f = open("data/exchange_rate.info", encoding="utf8")
-        st = f.read()
+        f = open('data/exchange_rate.info', 'r')
+        currency = []
+        for text in f:
+            if not text == '':
+                element = text.split('=')
+                currency.append( (element[0], float(element[1]) ) )
+
         f.close()
-        return st
+        self.exchange_rate = dict(currency)
+
+    def change_from(self, index: str):
+        if not self.from_currency == str and not self.to_currency == index:
+            self.from_currency = index
+
+    def change_to(self, index: str):
+        if not self.from_currency == str and not self.to_currency == index:
+            self.to_currency = index
 
     def __str__(self):
         st = '{\n'
 
         for rate in self.exchange_rate:
-            st = f'{st}\t"{rate}": {self.exchange_rate[rate]}'
+            st = f'{st}    "{rate}": {self.exchange_rate[rate]}'
             if not rate == list( self.exchange_rate.keys() )[-1]:
                 st = f'{st},\n'
             else:
