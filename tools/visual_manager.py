1c14b03dbe209166054819eb5330f38e6464a04b
JohnBordea <49273333+JohnBordea@users.noreply.github.com>
2021-12-31 15:18:11 +0200

    Implementarea Functiei text_edited()
    text_edited() - daca unul din widget-uri este editat, atunci celalalt va fi editat.

--- tools/visual_manager.py
+++ tools/visual_manager.py
@@ -6,25 +6,32 @@ class Window:
     master = None
     converter = None
     components = []
+    interaction_at = None
 
     def __init__(self):
         self.master = tkr.Tk()
         self.master.geometry("800x600")
         self.master.title("BRD Exchange")
         self.converter = Converter()
-        self.components.append( tkr.Label(text="") )
-        self.components.append( tkr.Text(self.master, height=1, width=25, bg='light cyan') )
+        self.components.append( tkr.Text(self.master, height=1, width=25, bg='light cyan', bd=0) )
+        self.components.append( tkr.Text(self.master, height=1, width=25, bg='light cyan', bd=0) )
+        f = tkr.Text(self.master, height=1, width=25, bg='light cyan')
+        #f.delete()
+        #self.components.append( tkr.Label(text="") )
 
-    def text_edited(self, e):
-        self.components[0].config( text=self.components[1].get(1.0,tkr.END) )
-        pass
+    def text_edited(self, event, index_from, index_to):
+        self.components[index_to].delete(1.0, 'end')
+        self.components[index_to].insert(1.0, self.components[index_from].get(1.0,tkr.END))
+        if index_from != self.interaction_at:
+            self.interaction_at = 0
+        #print(self.components[0])
 
     def set_contents(self):
 
-        self.components[1].bind('<KeyRelease>', self.text_edited)
+        self.components[0].bind( '<KeyRelease>', lambda event: self.text_edited_upper(event, 0, 1) )
 
-        self.components[0].grid(row=1,column=0)
-        self.components[1].grid(row=0,column=0)
+        self.components[0].grid(row=0,column=0)
+        self.components[1].grid(row=1,column=0)
         return self
 
     def show_window(self):
